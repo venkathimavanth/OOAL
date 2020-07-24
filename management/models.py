3cdba4c72f17ec377cd09754282b3224ad8ddeff
mrajeswarasai <50444037+mrajeswarasai@users.noreply.github.com>
2020-07-25 00:55:38 +0530

    Update

--- management/models.py
+++ management/models.py
@@ -1,16 +1,16 @@
-from mongoengine import connect,ObjectIdField,StringField,IntField,ListField,ImageField,DateTimeField, BooleanField, Document
+from mongoengine import connect,ObjectIdField,StringField,IntField,ListField,FileField,ImageField,DateTimeField, BooleanField, Document
 
 connect("EAD_OOAL")
 
 class CategoryModel(Document):
-    category_name = StringField(max_length=200,unique=True)
+    category_name = StringField(max_length=200)
 
 class ChallangeTypeModel(Document):
-    challange_type_model = StringField(max_length=200,unique=True)
+    challange_type_model = StringField(max_length=200)
 
 
 class TypeOfSubmissionModel(Document):
-    type_of_submission = StringField(max_length=200,unique=True)
+    type_of_submission = StringField(max_length=200)
 
 
 class Challange(Document):
@@ -33,3 +33,24 @@ class DailyChallanges(Document):
     number_of_reports = IntField(default=0)
     age_restricted=BooleanField(default=False)
     discription = StringField(max_length=1000)
+
+class WeeklyChallanges(Document):
+    name = StringField(max_length=200)
+    created_date=DateTimeField()
+    posted_year=StringField(max_length=200)
+    posted_week=StringField(max_length=200)
+    number_of_reports = IntField(default=0)
+    age_restricted=BooleanField(default=False)
+    discription = StringField(max_length=1000)
+
+class FunContent(Document):
+    created_date = DateTimeField()
+    created_user= ObjectIdField()
+    content = FileField()
+    description = StringField(max_length=1000)
+
+class Report(Document):
+    user_id = ObjectIdField
+    time = DateTimeField
+    post_id = ObjectIdField
+    problem = StringField(max_length=1000)
