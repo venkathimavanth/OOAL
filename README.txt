a910747361abe5eef133a193f04c739513e0d008
mrajeswarasai <50444037+mrajeswarasai@users.noreply.github.com>
2020-07-25 08:59:56 +0530

    update

---
+++ README.txt
@@ -0,0 +1,34 @@
+Steps:
+
+Step 1:
+
+Install python and pip in your computer
+
+Step 2:
+
+Install requirements by using below command
+'pip install -r requirements.txt'
+
+Step 3:
+
+Install MongoDB Compass in your Computer
+
+Step 4:
+
+Connect to localhost in MongoDB Compass
+
+Step 5:
+
+Create a DATABASE with name 'EAD_OOAL'
+
+Step 6:
+
+run the following commands
+
+python manage.py makemigrations
+python manage.py migrate
+python manage.py runserver
+
+Step 7:
+
+Open http://127.0.0.1:8000/ in your browser.....
--- user/views.py
+++ user/views.py
@@ -425,6 +425,396 @@ def lo(request,id):
         pass
     return redirect('user:userhome')
 
+    if request.method == "POST":
+        if "tought" in request.POST :
+            post=Post(
+                user_id =  user["id"],
+                # user_photo = ImageField()
+                created_date=datetime.datetime.now(),
+
+                # isimage = BooleanField(default=False)
+                # isvideo = BooleanField(default=False)
+
+                text = request.POST["tought"],
+                # image = ImageField()
+                # text = StringField(max_length=200)
+
+            )
+            post.save()
+            addThisPost(request,post,user,profile)
+        HttpResponseRedirect("user.views.userhome")
+
+    context={'warning':"Logged in successfully"}
+    try:
+        daily_challange=DailyChallanges.objects.get(posted_date=datetime.date.today())
+        context['daily_challange']=daily_challange
+    except:
+        pass
+
+    try:
+        weekNumber = date.today().isocalendar()[1]
+        Weekly_challange=reversed(WeeklyChallanges.objects.filter(posted_week=str(weekNumber)))
+        for w in Weekly_challange:
+            context['weekly_challange']=w
+            break
+
+    except:
+        pass
+
+    try:
+        ftf=profile["accepted_chall"]
+        lis=[]
+        for f in ftf:
+            lis.append(FriendToFriend.objects.get(id=f))
+        context['ftf']=lis
+    except:
+        pass
+
+
+    try:
+        lo=Limited_Offer.objects.all()
+        lis=[]
+        for l in lo :
+            loc=Limited_Offer_Coupons.objects.get(id=l["coupons"][0])
+            if datetime.datetime.now() < loc["expiry_date"]:
+                lis.append(l)
+        context['Limited_Offer']=lis
+    except:
+        pass
+
+
+    feed=profile["myfeed"]
+    posts=[]
+    for f in feed:
+        try:
+            p=Post.objects.get(id=f)
+            po=dict()
+            po["id"]=p["id"]
+            po["user_id"]=p["user_id"]
+            profile1=Profile.objects.get(user_id=p["user_id"])
+            po["username"]=profile1["name"]
+
+            photo= profile1["photo"].grid_id
+            col = db.images.chunks.find({"files_id":photo})
+            my_string = base64.b64encode(col[0]["data"])
+            po["user_photo"]=my_string.decode('utf-8')
+            # print(my_string.decode('utf-8'))
+            po["created_date"]=p["created_date"]#
+            po["isimage"]=p["isimage"]
+            po["isvideo"]=p["isvideo"]
+            po["text"]=p["text"]
+
+            content = p.content.read()
+            base64EncodedStr = base64.b64encode(content)
+            content = base64EncodedStr.decode('utf-8')
+            po["content"] =content
+
+            po["ischallenge"]=p["ischallenge"]
+
+            if p["ischallenge"]:
+                po["challegetype"]=p["challegetype"]
+                po["challengeid"]=p["challengeid"]
+
+
+            po["isad"]=p["isad"]
+
+            if p["isad"]:
+                po["adid"]=p["adid"]
+
+            po["likes"]=p["likes"]
+            po["comments"]=p["comments"]
+            posts.append(po)
+        except:
+            pass
+    context["posts"]=posts[-1:-101:-1]
+    context["sfriends"]=returnSuggestedFriends(request)
+
+    # print(context)
+    page = request.GET.get('page', 1)
+    paginator = Paginator(posts, 2)
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+
+
+    return render(request,'registration/loginhome.html',context)
+
+
+
+@login_required
+def single_post(request):
+    if request.method == "GET":
+        print("came into single_post")
+        post_id = request.GET["postId"]
+
+        username = request.session["username"]
+        user = User.objects(email=username)[0]
+        profile = Profile.objects.get(user_id=user["id"])
+
+        po=dict()
+
+        p=Post.objects.get(id=post_id)
+
+        po["id"]=p["id"]
+        po["user_id"]=p["user_id"]
+        profile1=Profile.objects.get(user_id=p["user_id"])
+        po["username"]=profile1["name"]
+
+        photo= profile1["photo"].grid_id
+        col = db.images.chunks.find({"files_id":photo})
+        my_string = base64.b64encode(col[0]["data"])
+        po["user_photo"]=my_string.decode('utf-8')
+        # print(my_string.decode('utf-8'))
+        po["created_date"]=p["created_date"]#
+        po["isimage"]=p["isimage"]
+        po["isvideo"]=p["isvideo"]
+        po["text"]=p["text"]
+
+        content = p.content.read()
+        base64EncodedStr = base64.b64encode(content)
+        content = base64EncodedStr.decode('utf-8')
+        po["content"] =content
+
+        po["ischallenge"]=p["ischallenge"]
+
+        if p["ischallenge"]:
+            po["challegetype"]=p["challegetype"]
+            po["challengeid"]=p["challengeid"]
+
+
+        po["isad"]=p["isad"]
+
+        if p["isad"]:
+            po["adid"]=p["adid"]
+
+        po['likedBy'] = p['likes']
+        po['is_liked_by_curr_user'] = False
+
+        if user['id'] in po['likedBy']:
+            po['is_liked_by_curr_user'] = True
+        po['likes'] = len(po['likedBy'])
+
+
+        po["comments"]=p["comments"]
+        try:
+            pass
+        except:
+            pass
+
+        my_photo= profile["photo"].grid_id
+        my_col = db.images.chunks.find({"files_id":my_photo})
+        my_my_string = base64.b64encode(my_col[0]["data"])
+        po["ownerphoto"] = my_my_string.decode('utf-8')
+        comments = []
+
+        for k in po["comments"]:
+            comm = Comment.objects.get(id = k)
+            print(comm)
+            temp1 = dict()
+            temp1['comment'] = comm['message']
+            temp1['reports'] = comm['message']
+            o = comm['owner']
+            own = User.objects(id = o)[0]
+            own_p = Profile.objects.get(user_id = o)
+            temp1['owner'] = own_p['name']
+            photo= own_p["photo"].grid_id
+            ph = db.images.chunks.find({"files_id":photo})
+            ph_string = base64.b64encode(ph[0]["data"])
+            temp1["photo"] = ph_string.decode('utf-8')
+
+            comments.append(temp1)
+        count = len(comments)
+        print('-------------------------',po["is_liked_by_curr_user"])
+        return render(request, 'registration/modal.html', {'myphoto': my_photo,'post': po, 'comments': comments, 'count': count})
+    else:
+        return HttpResponse("failure")
+
+
+def createComment(request):
+    print("create comment")
+    if request.method == "POST":
+        ad_user = request.session["username"]
+        post_id  =  request.POST["p_id"]
+        cmnt = request.POST["msg"]
+        a_uid = User.objects(email=ad_user)[0]
+
+        comnt = Comment(message = cmnt, owner= a_uid['id'])
+        comnt.save()
+        Post.objects(id=post_id).update_one(push__comments=comnt["id"])
+        return HttpResponse('Added comment to the post')
+    else:
+        return HttpResponse('Failure')
+
+
+def like(request):
+    if request.method == 'GET':
+        post_id = request.GET['postId']
+        username = request.session["username"]
+        user = User.objects(email=username)[0]
+        profile = Profile.objects.get(user_id=user["id"])
+        post = Post.objects.get(id = post_id)
+
+        if user['id'] not in post['likes']:
+            post.likedBy.append(user["id"])
+            post.save()
+        else:
+            post.likedBy.remove(user["id"])
+            post.save()
+        return HttpResponse('Success')
+    else:
+        return HttpResponse(Failure)
+
+
+def addThisPost(request,post,user,profile):
+    profile["myposts"].append(post["id"])
+    profile["myfeed"].append(post["id"])
+    profile.save()
+    for f in profile["friends"]:
+        p=Profile.objects.filter(user_id=f)
+        print(p)
+        if p:
+            for q in p:
+                q["myfeed"].append(post["id"])
+                q.save()
+
+def dsc(request):
+    context=dict()
+    ae,user=check_user_exists(request,request.session["username"])
+    profile = Profile.objects.get(id = user["profileid"])
+    if request.method == "POST":
+        if 'file' in request.FILES:
+            file = request.FILES['file']
+            content_type = 'video/mp4'
+            daily_challange=DailyChallanges.objects.get(posted_date=datetime.date.today())
+            post=Post(
+                user_id =  user["id"],
+                user_photo = profile["photo"],
+                created_date=datetime.datetime.now(),
+
+                isimage = False,
+                isvideo = True,
+
+                text = daily_challange["discription"],
+                ischallenge = True,
+                challegetype="Daily Single Challange",
+                challengeid=daily_challange["id"],
+            ).save()
+            post.content.put(file,content_type=content_type)
+            post.save()
+            addThisPost(request,post,user,profile)
+            profile["completed"].append(daily_challange["id"])
+            profile.save()
+
+
+    try:
+        daily_challange=DailyChallanges.objects.get(posted_date=datetime.date.today())
+        context['daily_challange']=daily_challange
+        context["completed"] = False
+        if daily_challange["id"] in profile["completed"]:
+            context["completed"] =True
+        # print("-----------------",context["completed"],profile["completed"])
+    except:
+        pass
+
+    return render(request,'user/dsc.html',context)
+
+def ftf(request,id):
+    context=dict()
+    ae,user=check_user_exists(request,request.session["username"])
+    profile = Profile.objects.get(id = user["profileid"])
+    if request.method == "POST":
+        if 'file' in request.FILES:
+            f=FriendToFriend.objects.get(id=id)
+            file = request.FILES['file']
+            content_type = 'video/mp4'
+            post=Post(
+                user_id =  user["id"],
+                user_photo = profile["photo"],
+                created_date=datetime.datetime.now(),
+
+                isimage = False,
+                isvideo = True,
+
+                text = f["discription"],
+                ischallenge = True,
+                challegetype="Friend to Friend Challange",
+                challengeid=f["id"],
+            ).save()
+            post.content.put(file,content_type=content_type)
+            post.save()
+            addThisPost(request,post,user,profile)
+            profile["accepted_chall"].remove(f["id"])
+            profile.save()
+            return redirect('user:userhome')
+
+    try:
+        f=FriendToFriend.objects.get(id=id)
+        if f["id"] in profile["accepted_chall"]:
+            context["ftf"]=f
+            return render(request,'user/ftf.html',context)
+    except:
+        pass
+    return redirect('user:userhome')
+
+
+def lo(request,id):
+    context=dict()
+    ae,user=check_user_exists(request,request.session["username"])
+    profile = Profile.objects.get(id = user["profileid"])
+    if request.method == "POST":
+        if 'file' in request.FILES:
+            f=Limited_Offer.objects.get(id=id)
+            loc=Limited_Offer_Coupons.objects.get(id=f["coupons"][0])
+
+            file = request.FILES['file']
+            content_type = 'video/mp4'
+            post=Post(
+                user_id =  user["id"],
+                user_photo = profile["photo"],
+                created_date=datetime.datetime.now(),
+
+                isimage = False,
+                isvideo = True,
+
+                text = loc["discription"],
+                ischallenge = True,
+                challegetype="Limited Offer",
+                challengeid=f["id"],
+            ).save()
+            post.content.put(file,content_type=content_type)
+            post.save()
+            addThisPost(request,post,user,profile)
+
+            cupon=Cupon(
+                offerid = f["id"],
+                couponid = loc["id"]
+            )
+            cupon.save()
+            profile["cupons"].append(cupon["id"])
+            profile.save()
+            return redirect('user:userhome')
+
+    try:
+        context=dict()
+        f=Limited_Offer.objects.get(id=id)
+        loc=Limited_Offer_Coupons.objects.get(id=f["coupons"][0])
+        if datetime.datetime.now() < loc["expiry_date"]:
+            context['offer']=f
+            context['cupon']=loc
+        completed=False
+        for c in profile["cupons"]:
+            cou=Cupon.objects.get(id=c)
+            if f.id == cou.offerid:
+                completed=True
+        context["completed"]=completed
+        return render(request,'user/lo.html',context)
+    except:
+        pass
+    return redirect('user:userhome')
+
 
 @login_required
 def friends(request):
